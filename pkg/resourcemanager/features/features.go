f65b940299b3f101400e64294d6890aa4f56166e
Seshachalam Yerasala Venkata <seshachalam.yerasala.venkata@sap.com>
2026-04-04 16:22:58 +0530

    feat(resourcemanager): register NodeReadinessController feature gate in resource-manager binary

---
+++ pkg/resourcemanager/features/features.go
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package features
+
+import (
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/component-base/featuregate"
+
+	"github.com/gardener/gardener/pkg/features"
+)
+
+// RegisterFeatureGates registers the feature gates of gardener-resource-manager.
+func RegisterFeatureGates() {
+	utilruntime.Must(features.DefaultFeatureGate.Add(features.GetFeatures(GetFeatures()...)))
+}
+
+// GetFeatures returns all gardener-resource-manager features.
+func GetFeatures() []featuregate.Feature {
+	return []featuregate.Feature{
+		features.NodeReadinessController,
+	}
+}
